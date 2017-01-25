95b151178fba77cac17aeebd155ad2a775f03319
Luciano <luchob.32@gmail.com>
2017-01-25 18:41:30 -0300

    AfipBill module

---
+++ lib/afip_bill.rb
@@ -0,0 +1,20 @@
+require 'afip_bill/check_digit'
+require 'afip_bill/generator'
+require 'afip_bill/line_item'
+require 'afip_bill/user'
+require 'afip_bill/version'
+
+module AfipBill
+  def self.configuration
+    @configuration ||= {
+      business_name: nil,
+      business_address: nil,
+      business_start_date: nil,
+      business_cuit: nil,
+      city: nil,
+      ingresos_brutos: nil,
+      iva: nil,
+      sale_point: nil,
+    }
+  end
+end
