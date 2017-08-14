4528d73b6fa770f2fd0b2a79937a82c930530e24
Emanuel Friedrich <aemanuelfriedrich@gmail.com>
2017-08-14 08:50:14 -0300

    Add "11" to BRAVO_CBTE_TIPO

--- lib/afip_bill/generator.rb
+++ lib/afip_bill/generator.rb
@@ -11,7 +11,7 @@ module AfipBill
 
     HEADER_PATH = File.dirname(__FILE__) + '/views/shared/_factura_header.html.erb'.freeze
     FOOTER_PATH = File.dirname(__FILE__) + '/views/shared/_factura_footer.html.erb'.freeze
-    BRAVO_CBTE_TIPO = { "01" => "Factura A", "06" => "Factura B" }.freeze
+    BRAVO_CBTE_TIPO = { "01" => "Factura A", "06" => "Factura B" , "11" => "Factura C"}.freeze
     IVA = 21.freeze
 
     def initialize(bill, user, line_items = [])
