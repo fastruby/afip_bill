e0271ff4c961191330bf088cc887758a3fdfe3dd
Mauro Otonelli <mauro.otonelli@gmail.com>
2018-01-21 21:59:00 -0300

    Allow to change 'ORIGINAL' text in header

--- lib/afip_bill/generator.rb
+++ lib/afip_bill/generator.rb
@@ -7,20 +7,21 @@ require "pdfkit"
 
 module AfipBill
   class Generator
-    attr_reader :afip_bill, :bill_type, :user, :line_items
+    attr_reader :afip_bill, :bill_type, :user, :line_items, :header_text
 
     HEADER_PATH = File.dirname(__FILE__) + '/views/shared/_factura_header.html.erb'.freeze
     FOOTER_PATH = File.dirname(__FILE__) + '/views/shared/_factura_footer.html.erb'.freeze
     BRAVO_CBTE_TIPO = { "01" => "Factura A", "06" => "Factura B" }.freeze
     IVA = 21.freeze
 
-    def initialize(bill, user, line_items = [])
+    def initialize(bill, user, line_items = [], header_text = 'ORIGINAL')
       @afip_bill = JSON.parse(bill)
       @user = user
       @bill_type = type_a_or_b_bill
       @line_items = line_items
       @template_header = ERB.new(File.read(HEADER_PATH)).result(binding)
       @template_footer = ERB.new(File.read(FOOTER_PATH)).result(binding)
+      @header_text = header_text
     end
 
     def type_a_or_b_bill
