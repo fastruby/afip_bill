c53e2791ba58628138af365aec9c23a6c4d88978
Thomas Cordeu <tcordeu@fu.do>
2019-07-22 14:49:08 -0300

    Regression tests for AfipBill::LineItem attributes

--- spec/afip_bill/item_spec.rb
+++ spec/afip_bill/item_spec.rb
@@ -4,11 +4,27 @@ require 'afip_bill/line_item'
 describe AfipBill::LineItem do
   subject { described_class }
 
+  let(:item) { AfipBill::LineItem.new('Item', 1, 100) }
+
   describe '#new' do
     it 'must be created with name, quantity and imp_unitario' do
       item = AfipBill::LineItem.new('Item', 1, 100)
 
       expect(item).to be_an_instance_of AfipBill::LineItem
+    end
+  end
+
+  describe 'attributes' do
+    it 'has name' do
+      expect(item.name).to eq 'Item'
+    end
+
+    it 'has quantity' do
+      expect(item.quantity).to eq 1
+    end
+
+    it 'has imp_unitario' do
+      expect(item.imp_unitario).to eq 100
     end
   end
 end
